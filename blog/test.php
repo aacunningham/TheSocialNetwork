3c4c22b749fa14b08be8f3d794d19a4ba18eda61
isaacsanchez <isaacsanchez@mail.fresnostate.edu>
2014-10-14 15:34:24 -0700

    add test.php
    nothing inside, just testing

---
+++ blog/test.php
@@ -0,0 +1,27 @@
+<?php
+    require_once "../Layout/header.php";
+    
+    $blogs = array ();
+    if (!empty($_POST['choose'])) { //they've chosen a folder to view
+        $blogs = $blog->display ($_POST['fid']); //get blog info
+    }
+    $folderList = $folder->listFolders (); //get user's list of folders
+?>
+    
+<title>Blogs</title>
+
+<!-- Back Navigtion -->
+<a href="interface.php" target="_self">Home</a>
+
+<!-- Errors -->
+<?php if (!empty($blog->message)) : ?>
+    <h3><?php echo $blog->message; ?></h3>
+<?php endif; ?>
+
+<?php if (!empty($blogs)) : ?>
+    
+<h1>test from Isaac</h1>
+
+<?php endif; ?>
+
+<?php endif; ?>
