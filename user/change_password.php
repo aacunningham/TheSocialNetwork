8249e3c2a2b21cae0e4d60400b6d79a81a80c8be
ssburns <ssburns@gmail.com>
2014-10-30 19:10:55 -0700

    password challenge & change
    working, has a couple issues to figure out still.
    User session can be set thus making the user appear logged in. Details
    in user handling of guessing passwords. Need to create secret question
    at user creation.

--- user/change_password.php
+++ user/change_password.php
@@ -5,6 +5,49 @@
 
 <?php
     require_once "../layout/header.php";
-	if(!isset($_SESSION['attempts'])) {
-		$_SESSION['attempts'] = 0;
-	}
+	
+    if (!empty($_POST['submit'])) { //change password submitted
+        if (test_input($_POST['password1']) == test_input($_POST['password2'])) {
+            $user->password = test_input(password_hash($_POST['password1'], PASSWORD_BCRYPT));
+            $success = $user->update_password();
+        } else {
+            $user->message = "Error: passwords don't match";
+        }
+		
+		if ($success ) {
+			header('Location: /user/interface.php');	//send to users page
+		}
+    }	
+?>
+
+<!-- Back Navigtion -->
+<a href="interface.php" target="_self">Home</a>
+
+<!-- Heading -->
+<h1>Change password</h1>
+
+<!-- Errors -->
+<?php if (!empty($user->message)) : ?>
+    <h3><?php echo $user->message; ?></h3>
+<?php endif; ?>
+
+<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="POST">
+    <table>
+        <!-- Password -->
+        <tr>
+            <td><b>Password:</b></td>
+            <td><input required type="password" name="password1"></td>
+        </tr>
+        
+        <!-- Retype Password -->
+        <tr>
+            <td><b>Retype Password:</b></td>
+            <td><input required type="password" name="password2"></td>
+        </tr>
+        
+        <!-- Submit -->
+        <tr>
+            <td><input type="submit" name="submit"></td>
+        </tr>
+    </table>
+</form>
