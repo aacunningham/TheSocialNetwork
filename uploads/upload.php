cc40b4ecdc606b9e9a424a8dbc7d311c694a8c81
IsaacSanchez <isaacsanchez@mail.fresnostate.edu>
2014-11-09 00:30:07 -0800

    code change

--- uploads/upload.php
+++ uploads/upload.php
@@ -3,17 +3,26 @@
 if (isset($_POST['submit'])) {
     $j = 0; //Variable for indexing uploaded image 
 
-$target_path = "uploads/"; 
-    for ($i = 0; $i < count($_FILES['file']['name']); $i++) {
+	$target_path = "uploads/"; 
+for ($i = 0; $i < count($_FILES['file']['name']); $i++) {
 
-$validextensions = array("jpeg", "jpg", "png"); 
-$ext = explode('.', basename($_FILES['file']['name'][$i]));
-$file_extension = end($ext);
+	$validextensions = array("jpeg", "jpg", "png"); 
+	$ext = explode('.', basename($_FILES['file']['name'][$i]));
+	$file_extension = end($ext);
 
-$target_path = $target_path . md5(uniqid()) . "." . $ext[count($ext) - 1];
+	$target_path = $target_path . md5(uniqid()) . "." . $ext[count($ext) - 1];
         $j = $j + 1;      
       
-	  if (($_FILES["file"]["size"][$i] < 100000) 
+if (($_FILES["file"]["size"][$i] < 100000) 
                 && in_array($file_extension, $validextensions)) {
-            if (move_uploaded_file($_FILES['file']['tmp_name'][$i], $target_path)) {
+     if (move_uploaded_file($_FILES['file']['tmp_name'][$i], $target_path)) {
                 echo $j. ').<span id="noerror">Image uploaded successfully!.</span><br/><br/>';
+		} else {
+                echo $j. ').<span id="error">please try again!.</span><br/><br/>';
+            }
+        } else {
+            echo $j. ').<span id="error">***Invalid file Size or Type***</span><br/><br/>';
+        }
+    }
+}
+?>
