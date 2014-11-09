81a9ec7a0467fe7f1a19f4def05870727f815980
IsaacSanchez <isaacsanchez@mail.fresnostate.edu>
2014-11-09 00:25:11 -0800

    code change

--- uploads/upload.php
+++ uploads/upload.php
@@ -3,3 +3,17 @@
 if (isset($_POST['submit'])) {
     $j = 0; //Variable for indexing uploaded image 
 
+$target_path = "uploads/"; 
+    for ($i = 0; $i < count($_FILES['file']['name']); $i++) {
+
+$validextensions = array("jpeg", "jpg", "png"); 
+$ext = explode('.', basename($_FILES['file']['name'][$i]));
+$file_extension = end($ext);
+
+$target_path = $target_path . md5(uniqid()) . "." . $ext[count($ext) - 1];
+        $j = $j + 1;      
+      
+	  if (($_FILES["file"]["size"][$i] < 100000) 
+                && in_array($file_extension, $validextensions)) {
+            if (move_uploaded_file($_FILES['file']['tmp_name'][$i], $target_path)) {
+                echo $j. ').<span id="noerror">Image uploaded successfully!.</span><br/><br/>';
