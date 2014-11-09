2dc86a862fcc67b816d0759e0e86f91abc389f9f
IsaacSanchez <isaacsanchez@mail.fresnostate.edu>
2014-11-09 00:45:36 -0800

    Photo upload
    enhancements

---
+++ uploads/multiupload.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+		<title>Photo Uploads</title>
+		<!-------Including jQuery from google------>
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+        <script src="script.js"></script>
+		
+		<!-------Including CSS File------>
+        <link rel="stylesheet" type="text/css" href="style.css">
